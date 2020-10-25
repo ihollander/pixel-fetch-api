5a04d04d36997200889fe0119be5d365cf429e72
ihollander <ianholla@gmail.com>
2020-10-25 09:55:16 -0400

    update allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_cable.allowed_request_origins = ['ihollander.github.io']
+  config.action_cable.allowed_request_origins = ['https://ihollander.github.io']
 end
